b71c8a20135232b0eb1b3bd942682e0a378009ef
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-07-16 19:13:39 +0000

    Build the v1 of the interface, the skeletal structure of the web app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
+import {
+  Box,
+  Text,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Textarea,
+  VStack,
+  Stack,
+  Button,
+  Flex,
+  Grid,
+  GridItem
+} from '@chakra-ui/react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TextCard = (props) => {
+  return <GridItem
+      p={4}
+      boxShadow='lg'
+      bg={'gray.50'}
+      borderRadius={10}
+      colSpan={[4, 2, 4, null, 2]}
+    >
+    {/* Header with text and copy button  */}
+    <Flex justify='space-between' pb={2}>
+      <Text alignSelf={'center'} fontSize='xl' fontFamily={'sans-serif'} fontWeight={'bold'}>Split {props.number}</Text>
+      <Button alignSelf={'center'} variant='ghost' colorScheme={'yellow'}>Copy Text</Button>
+    </Flex>
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    {/* Actual Text */}
+    <Box>
+    Lorem consequatur harum atque iure molestias Cum maiores provident ad fugit minima quis.
+    ibusdam recusandae eligendi quos cum veritatis Nisi explicabo quo necessitatibus beatae
+    omnis ipsam Repellendus eveniet natus voluptates.
+    </Box>
+  </GridItem>
 }
 
-export default App
+export default function App () {
+  return <>
+    <Box px={[5, 10]}>
+      {/* BeloText Big Header  */}
+      <Text fontSize={['3xl', '4xl']} fontFamily={'cursive'} fontWeight="bold" textAlign={'center'} pt={[4]} pb={1}>BeloText</Text>
+      <Text fontSize={['medium', 'lg']} color='gray.600' fontWeight='bold' textAlign={['left', 'left', 'center']} pb={[10]}>
+        Are you tired of always typing in limits? Type all your text, and split them into the limits.
+      </Text>
+
+      <Stack direction={['column', null, null, 'row']} spacing={10}>
+        {/* Form Container with two inputs and one button */}
+        <VStack spacing={5} w={['100%', null, null, '50%', '33.33%']}>
+          <NumberInput w={'full'} >
+            <NumberInputField
+              borderColor={'twitter.600'}
+              borderWidth={'2px'}
+              _hover={{ borderColor: 'twiter.400' }}
+              placeholder={'Enter text limit'}
+            />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+
+          <Textarea
+            rows={15}
+            borderColor={'twitter.600'}
+            borderWidth={'2px'}
+            _hover={{ borderColor: 'twitter.400' }}
+            placeholder={'Enter / Paste your actual text here ...'}
+          />
+
+          {/* Button to Perform the magic  */}
+          <Button colorScheme={'facebook'}>Make Magic Happen</Button>
+        </VStack>
+
+        {/* The Split Texts in Cards  */}
+        <Grid w={['100%', null, null, '50%', '66.66%']} gap={5} pb={10} templateColumns={'repeat(4, 1fr)'}>
+          {
+            [1, 2, 3, 4, 5].map((number) => (
+              <TextCard number={number} key={number} />
+            ))
+          }
+      </Grid>
+      </Stack>
+    </Box>
+  </>
+}
